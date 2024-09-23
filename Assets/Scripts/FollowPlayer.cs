9bf64c91156b7842d1ee36e3f502637f7d51e45c
ajdude9 <adamtheteeth@hotmail.com>
2024-09-23 18:44:57 +0100

    Serilisation and optimisation changes from Lesson 6.1

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    public Vector3 offset = new Vector3(0, 6, -14);
+    [SerializeField] private Vector3 offset = new Vector3(0, 6, -14);
     // Start is called before the first frame update
+    /**
     void Start()
     {
         
     }
-
+    */
     // Update is called once per frame
     void LateUpdate()
     {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20;// The speed at which the car moves forward and backwards
-    private float turnSpeed = 100;// The speed at which the car turns left or right
+    [SerializeField] protected float speed = 20;// The speed at which the car moves forward and backwards
+    [SerializeField] private float turnSpeed = 100;// The speed at which the car turns left or right
     private float horizontalInput;// A numerical value between -1 and 1, which decreases or increases based on player input ('A' or 'D' for Left and Right)
     private float verticalInput;// A numerical value between -1 and 1, which decreases or increases based on player input ('W' or 'S' for Up/Forward and Down/Backward)
     private float boostInput;// A numerical value between -1 and 1, which decreases or increases based on player input ('Space' to boost)
     public Camera mainCamera;
     public Camera hoodCamera;
     // Start is called before the first frame update
+    /**
     void Start()
     {
         
     }
-
+    */
     // Update is called once per frame
-    void Update() //Move the vehicle forward
+    void FixedUpdate() //Move the vehicle forward
     {
         horizontalInput = Input.GetAxis("Horizontal");// Bind the value of horizontalInput to the Horizontal axis, bound to A and D
         verticalInput = Input.GetAxis("Vertical");// Bind the value of verticalInput to the Vertical axis, bound to W and S
