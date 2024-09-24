06eab9795e81bb1b22f5e1123a8c394e22e1908c
ajdude9 <adamtheteeth@hotmail.com>
2024-09-24 18:54:58 +0100

    Lesson 6.2 changes; wheel colliders, on-ground logic, speedometer and RPM tracking, and relative force

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,45 +2,98 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] protected float speed = 20;// The speed at which the car moves forward and backwards
+     private float horsePower = 750;// The speed at which the car moves forward and backwards
     [SerializeField] private float turnSpeed = 100;// The speed at which the car turns left or right
     private float horizontalInput;// A numerical value between -1 and 1, which decreases or increases based on player input ('A' or 'D' for Left and Right)
     private float verticalInput;// A numerical value between -1 and 1, which decreases or increases based on player input ('W' or 'S' for Up/Forward and Down/Backward)
     private float boostInput;// A numerical value between -1 and 1, which decreases or increases based on player input ('Space' to boost)
     public Camera mainCamera;
     public Camera hoodCamera;
+    private Rigidbody playerRb;
+    [SerializeField] GameObject centreOfMass;
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] TextMeshProUGUI rpmText;
+    [SerializeField] float MPH;
+    [SerializeField] float RPM;
+
+    [SerializeField] List<WheelCollider> allWheels;
+    [SerializeField] int wheelsOnGround;
+
     // Start is called before the first frame update
-    /**
+    
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>();  
+        playerRb.centerOfMass = centreOfMass.transform.position; 
         
     }
-    */
+    
     // Update is called once per frame
     void FixedUpdate() //Move the vehicle forward
     {
-        horizontalInput = Input.GetAxis("Horizontal");// Bind the value of horizontalInput to the Horizontal axis, bound to A and D
-        verticalInput = Input.GetAxis("Vertical");// Bind the value of verticalInput to the Vertical axis, bound to W and S
-        boostInput = Input.GetAxis("Jump");//Bind the value of boostInput to the 'Jump' axis, bound to Space
-        if(boostInput > 0)
+        if(Grounded())
         {
-            speed = 40;
+            PlayerMovementController();   
         }
-        else
+    }
+    void Update()
+    {
+        CameraController();
+        if(Grounded())
         {
-            speed = 20;
+            DisplayController();
         }
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);// Move the car forward linearly every second, and multiply it by the speed and vertical input to allow it to only move when W or S is pressed
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);// Turn the car on the Y axis, rotating its model, based on whether the player is holding Left (A) or Right (D)
+    }
+
+    void CameraController()//Controls the Camera switching
+    {
         if(Input.GetKeyDown("r"))
         {
             mainCamera.enabled = !mainCamera.enabled;
             hoodCamera.enabled = !hoodCamera.enabled;
         }
+    }
 
-       
+    void DisplayController()
+    {
+        MPH = Mathf.Round(playerRb.velocity.magnitude * 2.237f);
+        speedometerText.SetText("MPH: " + MPH);
+        RPM = Mathf.Round((MPH % 30)*40);
+        rpmText.SetText("RPM: " + RPM);
+    }
+
+    void PlayerMovementController()//Handles player movement
+    {
+        horizontalInput = Input.GetAxis("Horizontal");// Bind the value of horizontalInput to the Horizontal axis, bound to A and D
+        verticalInput = Input.GetAxis("Vertical");// Bind the value of verticalInput to the Vertical axis, bound to W and S
+        boostInput = Input.GetAxis("Jump");//Bind the value of boostInput to the 'Jump' axis, bound to Space        
+
+        playerRb.AddRelativeForce(Vector3.forward * verticalInput * (horsePower * 100));
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);// Turn the car on the Y axis, rotating its model, based on whether the player is holding Left (A) or Right (D)
+    }
+
+    bool Grounded()
+    {
+        wheelsOnGround = 0;
+        foreach(WheelCollider wheel in allWheels)
+        {
+            if(wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+        if(wheelsOnGround >= 2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
